b21e4917105a0bdcba438caab4b2e40ca620763c
Davi Machado <brisolo@proton.me>
2025-04-23 22:32:53 -0300

    first commit again

---
+++ src/layouts/Project/ProjectList.ts
@@ -0,0 +1,17 @@
+export const projectList = [
+	{
+		name: "Emerald",
+		desc: "Um bot de uso geral para o Discord, com comandos úteis, moderação, diversão e mais.",
+		url: "https://github.com/argonptg/emerald",
+	},
+	{
+		name: "BPO",
+		desc: "Um aplicativo simples e intuitivo para organizar e gerenciar sua biblioteca de jogos.",
+		url: "https://github.com/blackpearlorigin/blackpearlorigin",
+	},
+	{
+		name: "PocketBaseSharp",
+		desc: "Um wrapper moderno em C#/.NET para facilitar a integração com o PocketBase (em desenvolvimento ativo).",
+		url: "https://github.com/argonptg/PocketBaseSharp",
+	},
+];
