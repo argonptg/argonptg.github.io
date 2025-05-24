accee5f45f9b6d8cc700bc585ec0cf84807f0c41
Davi Machado <brisolo@proton.me>
2025-05-24 14:25:50 -0300

    responsive website!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://argonptg.uk",
-	markdown: {
-		shikiConfig: {
-			theme: "catppuccin-mocha",
-		},
-	},
-	integrations: [mdx()],
+  site: "https://argonptg.uk",
+  markdown: {
+    shikiConfig: {
+      theme: "catppuccin-mocha",
+    },
+  },
+  integrations: [mdx()],
+  adapter: cloudflare(),
 });
